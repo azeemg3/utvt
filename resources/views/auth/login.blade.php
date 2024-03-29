33eb3cbb87c30dcae9ece16bb7a1cd4417cb72b0
azeemnowasys <azeem.khalid@nowasys.com>
2024-03-29 09:56:23 +0500

    update loginpage

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Mulholland Scrap Metals Ltd | Log in</title>
+    <title>Tour Vision Travel Pvt Ltd | Log in</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
